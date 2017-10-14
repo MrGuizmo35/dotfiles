61ef6b5b0c7b2c131daf1ce437cbef3f6e0582b1
Guillaume CORU <guillaume@MacBook-Air-de-Guillaume.local>
2017-10-14 17:14:29 +0200

    edited .emacs. Now great on mac.

--- .emacs
+++ .emacs
@@ -1,82 +1,64 @@
-
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
 (package-initialize)
-(define-key minibuffer-local-completion-map "\M-n" (lambda () (interactive) (insert "~")))
+;;emacs is broken on mac without this
+(setq mac-option-key-is-meta t)
+(setq mac-right-option-modifier nil)
+;;melpa repo
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'load-path "~/.emacs.d/evil")
+;;evil mode!
+(require 'evil)
+(evil-mode 1)
+
+(global-undo-tree-mode)
+;;helm fuzzy completion on mini-buffer
+(require 'helm-config)
+(helm-mode 1)
+
+;;relative numbering
+(require 'linum-relative)
+(linum-relative-global-mode 1)
+
+;;snippets manager
+(require 'yasnippet)
+(yas-global-mode 1)
+
+;;aggressive indent
+(global-aggressive-indent-mode 1)
+
+;;auto-complete
+(ac-config-default)
+
+;;powerline
+(require 'powerline)
+(powerline-evil-vim-theme)
+
+;;hooks rainbow delimiters to prog mode
+(add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
+
+;;correct tabs and indent style
+(setq c-default-style "linux"
+      c-basic-offset 4
+      tab-width 4
+      indent-tabs-mode t)
+;;set theme
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(cua-global-mark-cursor-color "#2aa198")
- '(cua-normal-cursor-color "#839496")
- '(cua-overwrite-cursor-color "#b58900")
- '(cua-read-only-cursor-color "#859900")
+ '(blink-cursor-mode nil)
+ '(cursor-in-non-selected-windows nil)
  '(custom-enabled-themes (quote (monokai)))
  '(custom-safe-themes
    (quote
-    ("8ed752276957903a270c797c4ab52931199806ccd9f0c3bb77f6f4b9e71b9272" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
- '(helm-mode t)
- '(highlight-symbol-colors
-   (--map
-    (solarized-color-blend it "#002b36" 0.25)
-    (quote
-     ("#b58900" "#2aa198" "#dc322f" "#6c71c4" "#859900" "#cb4b16" "#268bd2"))))
- '(highlight-symbol-foreground-color "#93a1a1")
- '(hl-bg-colors
-   (quote
-    ("#7B6000" "#8B2C02" "#990A1B" "#93115C" "#3F4D91" "#00629D" "#00736F" "#546E00")))
- '(hl-fg-colors
-   (quote
-    ("#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36")))
- '(nrepl-message-colors
-   (quote
-    ("#dc322f" "#cb4b16" "#b58900" "#546E00" "#B4C342" "#00629D" "#2aa198" "#d33682" "#6c71c4")))
- '(package-selected-packages
-   (quote
-    (magit monokai-theme powerline-evil powerline ggtags auto-complete aggressive-indent yasnippet fiplr linum-relative rainbow-delimiters helm undo-tree solarized-theme)))
- '(smartrep-mode-line-active-bg (solarized-color-blend "#859900" "#073642" 0.2))
- '(term-default-bg-color "#002b36")
- '(term-default-fg-color "#839496")
- '(vc-annotate-background-mode nil)
- '(xterm-color-names
-   ["#073642" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#eee8d5"])
- '(xterm-color-names-bright
-   ["#002b36" "#cb4b16" "#586e75" "#657b83" "#839496" "#6c71c4" "#93a1a1" "#fdf6e3"]))
+	("8ed752276957903a270c797c4ab52931199806ccd9f0c3bb77f6f4b9e71b9272" default))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-(add-to-list 'load-path "~/.emacs.d/evil")
-(require 'evil)
-(evil-mode 1)
 
-(global-undo-tree-mode)
-(require 'helm-config)
-(helm-mode 1)
-
-(require 'linum-relative)
-(linum-relative-global-mode 1)
-
-(require 'yasnippet)
-(yas-global-mode 1)
-
-(global-aggressive-indent-mode 1)
-
-(ac-config-default)
-
-(require 'powerline)
-(powerline-evil-vim-theme)
-
-(add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
-
-(setq c-default-style "linux"
-      c-basic-offset 4
-      tab-width 4
-      indent-tabs-mode t)
+;;global highlight current line
+(global-hl-line-mode 1)
---
+++ .emacs.d/SessionDesktop.el
@@ -0,0 +1,34 @@
+;; -*- mode: emacs-lisp; coding: emacs-mule; -*-
+;; --------------------------------------------------------------------------
+;; Desktop File for Emacs
+;; --------------------------------------------------------------------------
+;; Created Sat Oct 14 16:13:21 2017
+;; Desktop file format version 208
+;; Emacs version 25.1.1
+(setq revive:frame-configuration-to-restore
+  '(progn 
+     (revive:restore-frame '((tool-bar-position . top) (explicit-name) (icon-name) (bottom-divider-width . 0) (right-divider-width . 0) (top . 65) (left . 276) (unsplittable) (modeline . t) (width . 114) (height . 31) (fringe) (powerline-cache . #s(hash-table size 65 test equal rehash-size 1.5 rehash-threshold 0.8 data ())) (mouse-color . "black") (environment) (visibility . t) (cursor-color . "Red") (background-mode . dark) (fullscreen) (alpha) (scroll-bar-height . 0) (scroll-bar-width . 15) (cursor-type . box) (auto-lower) (auto-raise) (icon-type) (title) (buffer-predicate) (tool-bar-lines . 1) (menu-bar-lines . 1) (right-fringe . 8) (left-fringe . 1) (line-spacing) (background-color . "#2e3436") (foreground-color . "Black") (horizontal-scroll-bars) (vertical-scroll-bars . right) (internal-border-width . 0) (border-width . 0) (font . "-*-Monaco-normal-normal-normal-*-12-*-*-*-m-0-iso10646-1") (fontsize . 0)) '(114 31 ((0 0 117 30)) (("~/Library/Application Support/Aquamacs Emacs/scratch buffer" "*scratch*" 1 1 ((tab "*scratch*" "/Users/guillaume/Library/Application Support/Aquamacs Emacs/scratch buffer"))))))))
+
+;; Global section:
+(setq desktop-saved-frameset [frameset 1 (23010 7041 799749 0) (desktop . "208") "guillaume@MacBook-Air-de-Guillaume.local" nil nil ((((font-backend mac-ct) (fontsize . 0) (font . "-*-Monaco-normal-normal-normal-*-12-*-*-*-m-0-iso10646-1") (border-width . 0) (internal-border-width . 0) (vertical-scroll-bars . right) (horizontal-scroll-bars) (foreground-color . "Black") (background-color . "#2e3436") (line-spacing) (left-fringe . 1) (right-fringe . 8) (menu-bar-lines . 1) (tool-bar-lines . 1) (title) (icon-type) (auto-raise) (auto-lower) (cursor-type . box) (scroll-bar-width . 15) (scroll-bar-height . 0) (alpha) (fullscreen) (display-type . color) (background-mode . dark) (cursor-color . "Red") (visibility . t) (environment) (mouse-color . "black") (powerline-cache) (fringe) (frameset--id . "9A8B-DA01-79CF-0863") (frameset--mini t . t) (height . 31) (width . 114) (modeline . t) (minibuffer . t) (unsplittable) (left . 276) (top . 65) (right-divider-width . 0) (bottom-divider-width . 0) (icon-name) (display . "MacBook-Air-de-Guillaume.local") (explicit-name) (tool-bar-position . top)) ((min-height . 4) (min-width . 10) (min-height-ignore . 2) (min-width-ignore . 6) (min-height-safe . 1) (min-width-safe . 2) (min-pixel-height . 64) (min-pixel-width . 70) (min-pixel-height-ignore . 31) (min-pixel-width-ignore . 38) (min-pixel-height-safe . 16) (min-pixel-width-safe . 14)) leaf (pixel-width . 822) (pixel-height . 480) (total-width . 117) (total-height . 30) (normal-height . 1.0) (normal-width . 1.0) (buffer "*scratch*" (selected . t) (hscroll . 0) (fringes 1 8 nil) (margins nil) (scroll-bars nil 3 t nil 0 nil) (vscroll . 0) (dedicated) (point . 1) (start . 1))))])
+(setq desktop-missing-file-warning nil)
+(setq tags-file-name nil)
+(setq tags-table-list nil)
+(setq search-ring nil)
+(setq regexp-search-ring nil)
+(setq register-alist nil)
+(setq file-name-history '("~/dotfiles/.emacs.d/SessionDesktop.el" "~/dotfiles/.emacs" "~/.emacs" "~/dotfiles/.emacs" "~/.emacs" "~/dotfiles/.emacs" "~/.emacs" "~/.emac" "~/dotfiles/.emacs" "~/.emacs" "~/Library/Preferences/Aquamacs Emacs/Packages/elpa/auto-complete-20170124.1845/auto-complete-autoloads.el" "~/Library/Preferences/Aquamacs Emacs/Packages/elpa/solarized-theme-20170924.1440/solarized-theme-autoloads.el" "~/Library/Preferences/Aquamacs Emacs/Packages/elpa/dash-20171010.131/dash-autoloads.el" "~/Library/Preferences/Aquamacs Emacs/Packages/elpa/aggressive-indent-20171012.1107/aggressive-indent-autoloads.el" "~/Library/Preferences/Aquamacs Emacs/Packages/elpa/powerline-evil-20151112.710/powerline-evil-autoloads.el" "~/Library/Preferences/Aquamacs Emacs/Packages/elpa/evil-20171012.1414/evil-autoloads.el" "~/Library/Preferences/Aquamacs Emacs/Packages/elpa/goto-chg-20170917.1200/goto-chg-autoloads.el" "~/Library/Preferences/Aquamacs Emacs/Packages/elpa/powerline-20171010.713/powerline-autoloads.el" "~/Documents/projets/testPlatformio/src/main.cpp" "~/Library/Preferences/Aquamacs Emacs/Packages/elpa/monokai-theme-20171013.236/monokai-theme-autoloads.el" "~/dotfiles/.emacs" "~/.emacs" "~/Library/Preferences/Aquamacs Emacs/Packages/elpa/yasnippet-20170923.1646/yasnippet-autoloads.el" "~/Library/Preferences/Aquamacs Emacs/Packages/elpa/linum-relative-20160510.118/linum-relative-autoloads.el" "~/Library/Preferences/Aquamacs Emacs/Packages/elpa/helm-20171010.2317/helm-autoloads.el" "~/Library/Preferences/Aquamacs Emacs/Packages/elpa/popup-20160709.729/popup-autoloads.el" "~/Library/Preferences/Aquamacs Emacs/Packages/elpa/helm-core-20170929.2213/helm-core-autoloads.el" "~/Library/Preferences/Aquamacs Emacs/Packages/elpa/async-20170916.2256/async-autoloads.el" "~/Library/Preferences/Aquamacs Emacs/Packages/elpa/undo-tree-20170706.246/undo-tree-autoloads.el" "~/dotfiles/.emacs" "~/.emacs"))
+
+;; Buffer section -- buffers listed in same order as in buffer list:
+(desktop-create-buffer 208
+  "/Users/guillaume/Library/Application Support/Aquamacs Emacs/scratch buffer"
+  "*scratch*"
+  'text-mode
+  '(visual-line-mode smart-spacing-mode undo-tree-mode helm-mode linum-mode linum-relative-mode yas-minor-mode)
+  1
+  '(nil nil)
+  nil
+  nil
+  '((overwrite-mode) (truncate-lines))
+  '((mark-ring nil)))
+
